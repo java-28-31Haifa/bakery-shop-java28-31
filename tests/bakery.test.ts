623314adc0ab302e740c72714a0511eabf814c15
Kon Spirin <kon@mail.com>
2025-09-15 14:00:00 +0300

    hw40: tests CRUD category collection

--- tests/bakery.test.ts
+++ tests/bakery.test.ts
@@ -1,10 +1,11 @@
-import {isCategoryExists} from "../src/firebase/firebaseDBService";
+import {addCategory, isCategoryExists, removeCategory} from "../src/firebase/firebaseDBService";
 import {deleteApp} from 'firebase/app';
 import { app } from "../src/configurations/firebase-config";
+import {getRandomNumber} from "../src/utils/tools";
 
 
 describe('BakeryShop tests', () => {
-
+    const categories: string[] = ['bread', 'biscuits', 'croissants', 'cake', 'pizza']
     afterAll(async () => {
         await deleteApp(app);
     });
@@ -12,5 +13,25 @@ describe('BakeryShop tests', () => {
     test('Test:firebaseDbService.isCategoryExists', () => {
         expect(isCategoryExists('bread')).resolves.toBeTruthy();
         expect(isCategoryExists('milk')).resolves.toBeFalsy();
+    })
+
+    test('random category', async () => {
+        const category = categories[getRandomNumber(0, categories.length)]
+        await expect(isCategoryExists(category)).resolves.toBeTruthy();
+    })
+
+    test('all category exists', async () => {
+        const promiseArr = categories.map(cat => isCategoryExists(cat));
+        const resArr = await Promise.all(promiseArr);
+        const res = resArr.every(item => item);
+        expect(res).toBeTruthy();
+    })
+
+    test('remove add category', async () => {
+        const cat = 'bread'
+        await removeCategory(cat)
+        expect(await isCategoryExists(cat)).toBeFalsy()
+        await addCategory({categoryName: cat})
+        expect(await isCategoryExists(cat)).toBeTruthy()
     })
 })
